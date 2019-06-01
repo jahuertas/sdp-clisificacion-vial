d5044ce8471e672c1c35d988984da5b19f68d446
apalacio10 <40868159+apalacio10@users.noreply.github.com>
2019-06-01 17:40:04 -0500

    Envio 3

--- R Scripts/1. Procesamiento.R
+++ R Scripts/1. Procesamiento.R
@@ -14,21 +14,28 @@
   
   #Se cargan las capas (shapefiles)
   
-  layer_calzadas<- st_read(paste0(ruta_base_datos,"/Calz.shp"), stringsAsFactors = FALSE)
   layer_andenes <- st_read(paste0(ruta_base_datos,"/Ande.shp"), stringsAsFactors = FALSE)
+  layer_calzadas<- st_read(paste0(ruta_base_datos,"/Calz.shp"), stringsAsFactors = FALSE)
+  layer_cicloruta <-st_read(paste0(ruta_base_datos,"/Cicl.shp"), stringsAsFactors = FALSE) 
   layer_separadores <- st_read(paste0(ruta_base_datos,"/Sepa.shp"), stringsAsFactors = FALSE)
   
 #Procesamiento----
   
   #Procesamiento calzadas
   
+  andenes <- layer_andenes  %>% filter(st_is_valid(.))
   calzadas <- layer_calzadas %>% filter(st_is_valid(.))
+  cicloruta <-layer_cicloruta %>% filter(st_is_valid(.))
+  separados <-layer_separadores %>% filter(st_is_valid(.)) 
+  
+  
+  
   poligonos_calzadas <- calzadas %>% group_by(CalCIV) %>% summarise(N=n())
 
-  #
+  #Procesamiento andenes
   
   andenes <- layer_andenes  %>% filter(st_is_valid(.))
-
+  poligonos_andenes <- andenes %>% group_by(CalCIV) %>% summarise(N=n())
 
 
 
